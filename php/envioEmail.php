6dd93a9a029a0432be888eff61f6bb9c0a77270c
Bruno Souza <bruno1994deilson@gmail.com>
2021-05-19 21:21:23 -0300

    form action

--- php/envioEmail.php
+++ php/envioEmail.php
@@ -1,32 +1,12 @@
 <?php
-  //Variáveis
-  $nome = $_POST['nome'];
-  $email = $_POST['email'];
-  $mensagem = $_POST['mensagem'];
-  $data_envio = date('d/m/Y');
-  $hora_envio = date('H:i:s');
+   //Variáveis que recebem os dados digitados no formulário pelo id atribuído nos input
+   $nome = $POST[nome]; 
+   $email = $POST[email]; 
+   $mensagem = $POST[mensagem];
 
-  //Compo E-mail
-  $arquivo = "
-    <html>
-      <p><b>Nome: </b>$nome</p>
-      <p><b>E-mail: </b>$email</p>
-      <p><b>Mensagem: </b>$mensagem</p>
-      <p>Este e-mail foi enviado em <b>$data_envio</b> às <b>$hora_envio</b></p>
-    </html>
-  ";
-  
-  //Emails para quem será enviado o formulário
-  $destino = "bruno_dps@hotmail.com";
-  $assunto = "Contato pelo Site";
-
-  //Este sempre deverá existir para garantir a exibição correta dos caracteres
-  $headers  = "MIME-Version: 1.0\n";
-  $headers .= "Content-type: text/html; charset=iso-8859-1\n";
-  $headers .= "From: $nome <$email>";
-
-  //Enviar
-  mail($destino, $assunto, $arquivo, $headers);
-  
-  echo "<meta http-equiv='refresh' content='10;URL=../index.html'>";
+mail (
+    "bruno_dps@hotmail.com", //Endereço que vai receber a mensagem
+    "Nome: $nome
+     Email: $email   
+     Mensagem: $mensagem", "FROM:$nome<$email>");
 ?>
